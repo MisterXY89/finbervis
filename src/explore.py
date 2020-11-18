dfc5bf1714646a2ef2441bbe03afb7338f94a266
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-11-18 15:18:33 +0100

    adding tests

--- src/explore.py
+++ src/explore.py
@@ -22,6 +22,7 @@ def get_distribution(df):
     for value in values:
         length = len(df.query(f"sentiment == '{value}'"))
         print(f"amount({value}) = {length}")
+    return True
 
 
 def clean_dataset(df):
---
+++ test.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import pytest
+import pandas as pd
+
+sys.path.insert(0, './src')
+
+from config import test_df
+from explore import reduce_df, clean_dataset, flag_sentences, get_distribution
+
+
+
+################################################
+# TESTING EXPLORE.py
+
+def test_reduce_df():
+    df = reduce_df(test_df)
+    assert "segment" in df and "sentiment" and len(df.columns) == 2
+
+def test_clean_dataset():    
+    df = clean_dataset(test_df)
+    assert '\n' not in df.values and "\r" not in df.values
+
+def test_flag_dataset():
+    df = clean_dataset(test_df)
+    df = flag_sentences(df)
+    assert "isSentence" in df
+
+def test_get_distribution():
+    assert get_distribution(test_df) == True
+
