3788eeb81cd708deafc0410e9e11057349f87ba2
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-11-18 15:16:33 +0100

    fixing config

--- src/config.py
+++ src/config.py
@@ -4,11 +4,19 @@
 ---
 specifying constants and som general config
 """
+import os
+import pandas as pd
 
 # FILE CONSTANTS
-DATA_DIR = "data"
+WORKING_DIR = os.getcwd()
+BASE_DIR = WORKING_DIR if not "src" in WORKING_DIR else WORKING_DIR.split("/src")[0]
+DATA_DIR = BASE_DIR + "/data"
+
 
 DATASET_FILENAME = "hand_coded_text_segments.csv"
 DATASET_FILE = f"{DATA_DIR}/{DATASET_FILENAME}"
 CLEANED_DATASET_FILENAME = "text_segments_cleaned.csv"
 CLEANED_DATASET_FILE = f"{DATA_DIR}/{CLEANED_DATASET_FILENAME}"
+
+# TESTING DATA
+test_df = pd.read_csv(DATASET_FILE)
--- src/explore.py
+++ src/explore.py
@@ -5,11 +5,12 @@
 Descripton of explore.py
 """
 
-from config import *
+from config import DATASET_FILE, CLEANED_DATASET_FILE
 import pandas as pd
 
 
 def reduce_df(df):
+    df = df.rename({'Segment': 'segment'}, axis=1)
     return df[["segment", "sentiment"]]
 
 
@@ -27,11 +28,10 @@ def clean_dataset(df):
     """
 	expected: df is smaller & now new line can be found
 	"""
-    df = df.rename({'Segment': 'segment'}, axis=1)
     df_slim = reduce_df(df)
     # getting rid of newlines and multiple spaces
-    df_slim = df_slim.replace('(\r|\n)', ' ', regex=True)
-    df_slim = df_slim.replace('\s+', ' ', regex=True)
+    df_slim = df_slim.replace(r'(\r|\n)', ' ', regex=True)
+    df_slim = df_slim.replace(r'\s+', ' ', regex=True)
     return df_slim
 
 
@@ -39,7 +39,7 @@ def flag_sentences(df):
     """
 	there are matches in col, ther may be a wrong sentence up front or at the end
 	"""
-    sentence_regex = "[A-Z]+[a-zA-Z|\s|,|0-9]+[?|\!|.]+"
+    sentence_regex = r"[A-Z]+[a-zA-Z|\s|,|0-9]+[?|\!|.]+"
     df["isSentence"] = df.segment.str.contains(sentence_regex,
                                                regex=True,
                                                na=False)
@@ -53,7 +53,7 @@ def cleanSentences(df):
 # basic loading & cleaning
 df = pd.read_csv(DATASET_FILE)
 df_slim = clean_dataset(df)
-getDistribution(df_slim)
+get_distribution(df_slim)
 df_slim = flag_sentences(df_slim)
 
 print(30 * "-")
@@ -61,5 +61,5 @@ print(30 * "-")
 # cut of isSentence flag & store cleaned df
 df_slim_clean = df_slim.query(f"isSentence == True")
 df_slim_clean = reduce_df(df_slim_clean)
-getDistribution(df_slim_clean)
+get_distribution(df_slim_clean)
 df_slim_clean.to_csv(CLEANED_DATASET_FILE, index=False)
