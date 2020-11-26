ec6152808c2033280817de2fe98183c41f0f4692
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2020-11-26 15:34:39 +0100

    comments & dependency parsing

--- src/structure_analysis.py
+++ src/structure_analysis.py
@@ -7,6 +7,8 @@ Anylse the structure of the segments via n-grams & POS-Tagging
 
 import nltk
 import spacy
+from spacy import displacy
+
 import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -23,23 +25,34 @@ COLORS = [
 ]
 
 
+
+
 def get_pos_tags(segments:list) -> list:
+    """
+    return a list containing POS-tags for all tokens in the segment_list
+    """
     pos_tags = []
     print("Getting POS-Tags...")
     for segment in tqdm(segments):
         doc = nlp(segment)
         # doc_tags = []
         for token in doc:
-            pos_tags.append(token.pos_)
+            pos_tags.append(token.dep_)
         # POS_TAGS.append(doc_tags)
     return pos_tags
 
 def get_n_grams(list_of_values:list, n_value:int, limit:int =10):
+    """
+    find n-grams for the given list_of_values, mainly used for POS-tags
+    """
     return pd.Series(nltk.ngrams(list_of_values, n_value)).value_counts()[:limit]
 
 
 
 def value_pos_n_grams(n_value: int, limit: int) -> None:
+    """
+    POS-n-grams split by sentiment
+    """
     n_gram_series_dict = {}
     for value in LABEL_VALUES:
         print(f"POS-TAGS for <{value}>:")
@@ -52,6 +65,9 @@ def value_pos_n_grams(n_value: int, limit: int) -> None:
     return pd.DataFrame(n_gram_series_dict)
 
 def overall_pos_n_grams(n_value: int, limit: int):
+    """
+    overall POS-tags
+    """
     segments = list(data_frame.segment)
     pos_tags = get_pos_tags(segments)
     n_grams = get_n_grams(pos_tags, n_value, limit=limit)
@@ -61,13 +77,19 @@ def overall_pos_n_grams(n_value: int, limit: int):
 
 
 def plot_n_grams(n_grams_series, n_value: int, limit: int) -> None:
+    """
+    plot a horizonzal bar chart with the given series
+    """
     # sort_values()
-    n_grams_series.sort_values().plot.barh(color=["SkyBlue","IndianRed", "tab:gray"], width=.9, figsize=(12, 8))
+    n_grams_series.sort_values().plot.barh(color=["SkyBlue", "IndianRed", "tab:gray"][0], width=.9, figsize=(12, 8))
     plt.title(f"{limit} Most Frequently Occuring {n_value}grams")
     plt.ylabel(f"{n_value}-grams")
     plt.xlabel("# of Occurances")
     plt.show()
 
+
+# doc = nlp(data_frame.segment[0])
+# displacy.serve(doc, style="dep")
 
 N_GRAM_VALUE = 3
 LIMIT = 10
